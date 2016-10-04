4d96edc4bf985fb65083faf18b413ff36924b6a5
Deborah Hamel <deborahleehamel@gmail.com>
2016-10-03 22:34:17 -0600

    add idea model test

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Idea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    expect(Idea.new).to be_valid
+  end
+
+  it "is not valid without a title"
+  it "is not valid without a body"
+  it "is not valid without a quality"
 end
