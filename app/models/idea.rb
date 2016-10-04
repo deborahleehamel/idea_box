8c5083af0ad9930b21fedf7fd8fa10c2ef9d5ec3
Deborah Hamel <deborahleehamel@gmail.com>
2016-10-04 12:44:55 -0600

    add test for quality default

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,3 +1,3 @@
 class Idea < ApplicationRecord
-  
+  validates :title, presence: true
 end
--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -5,7 +5,16 @@ RSpec.describe Idea, type: :model do
     expect(Idea.new).to be_valid
   end
 
-  it "is not valid without a title"
-  it "is not valid without a body"
-  it "is not valid without a quality"
+  # it "is not valid without a title"
+  # it "is not valid without a body"
+  # it "is not valid without a quality"
+
+describe "#quality" do
+  it "initially defaults to swill" do
+    idea = ideas(:one)
+
+    expect(idea.quality).to eq("swill")
+  end
+end
+
 end
